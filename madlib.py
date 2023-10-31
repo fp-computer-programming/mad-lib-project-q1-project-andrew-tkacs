32072e0bc6e4b53f7d4716dbec2cb3e882d9cbd0
Aj Tkacs <p24atkacs@fairfieldprep.org>
2023-10-31 18:43:28 -0400

    m

--- madlib.py
+++ madlib.py
@@ -1,87 +1,75 @@
-#Author: Andrew Tkacs
-
-# Jesuit Madlib Project
-
+# Jesuit History and Mission
 jesuit_history = "The Society of Jesus, also known as the Jesuits, is a religious order founded by Saint Ignatius of Loyola in the 16th century. Jesuits are known for their dedication to education, missionary work, and social justice."
 
 # Verbs
-
-verb_rnge = int(2)
-verb = input("What are your 2 verbs? ")
+minrangev = int(2)
+verb = input("Enter 2 verbs (separated by a space): ")
 verbs = verb.split()
-while len(verbs) < verb_rnge:
-    verb = input("What are your 2 verbs? ")
+while len(verbs) < minrangev:
+    verb = input("Enter 2 verbs (separated by a space): ")
     verbs = verb.split()
 verbf = verbs[0:2]
 
 # Past Tense Verb
-
-psttns_verb = int(1)
-verbp = input("What is your past tense verb? ")
+minrangepv = int(1)
+verbp = input("Enter a past tense verb: ")
 verbps = verbp.split()
-while len(verbps) < psttns_verb:
-    verbp = input("What is your past tense verb? ")
+while len(verbps) < minrangepv:
+    verbp = input("Enter a past tense verb: ")
     verbps = verbp.split()
 verbpf = verbps[0:1]
 
 # Verbs Ending in ING
-
-ving_rng = int(5)
-verbing = input("What are your 5 verbs ending in 'ing'? ")
+minrangeving = int(5)
+verbing = input("Enter 5 verbs ending in 'ing' (separated by spaces): ")
 verbings = verbing.split()
-while len(verbings) < ving_rng:
-    verbing = input("What are your 5 verbs ending in 'ing'? ")
+while len(verbings) < minrangeving:
+    verbing = input("Enter 5 verbs ending in 'ing' (separated by spaces): ")
     verbings = verbing.split()
 verbingf = verbings[0:5]
 
 # Singular Nouns
-
-sing_noun = int(12)
-snoun = input("What are your 12 nouns? ")
+minrangesn = int(12)
+snoun = input("Enter 12 nouns (separated by spaces): ")
 snouns = snoun.split()
-while len(snouns) < sing_noun:
-    snoun = input("What are your 12 nouns? ")
+while len(snouns) < minrangesn:
+    snoun = input("Enter 12 nouns (separated by spaces): ")
     snouns = snoun.split()
 snounf = snouns[0:12]
 
 # Plural Noun
-
-pl_noun = int(1)
-pnoun = input("What is your plural noun? ")
+minrangepn = int(1)
+pnoun = input("Enter a plural noun: ")
 pnouns = pnoun.split()
-while len(pnouns) < pl_noun:
-    pnoun = input("What is your plural noun? ")
+while len(pnouns) < minrangepn:
+    pnoun = input("Enter a plural noun: ")
     pnouns = pnoun.split()
 pnounf = pnouns[0:1]
 
 # Adjectives
-
-adj_rng = int(20)
-adj = input("What are your 20 adjectives? ")
+minrangea = int(20)
+adj = input("Enter 20 adjectives (separated by spaces): ")
 adjs = adj.split()
-while len(adjs) < adj_rng:
-    adj = input("What are your 20 adjectives? ")
+while len(adjs) < minrangea:
+    adj = input("Enter 20 adjectives (separated by spaces): ")
     adjs = adj.split()
 adjf = adjs[0:20]
 
 # Names
-
-min_rng_name = int(2)
-name = input("What are your 2 names? ")
+minrangena = int(2)
+name = input("Enter 2 names (separated by spaces): ")
 names = name.split()
-while len(names) < min_rng_name:
-    name = input("What are your 2 names? ")
+while len(names) < minrangena:
+    name = input("Enter 2 names (separated by spaces): ")
     names = name.split()
 namesf = names[0:2]
 
 # Breaking the story down into their sentences
-
 sentence0 = f"Once upon a time in a {adjf[0]} faraway land, there lived a group of {snounf[0]} Jesuits who were known for their {adjf[1]} intelligence and {adjf[2]} dedication."
 sentence1 = f"They were {verbingf[0]} tirelessly to spread the message of {snounf[1]} knowledge and {snounf[2]} enlightenment."
-sentence2 = f"One day, a {adjf[3]} and {adjf[4]} young Jesuit named {namesf[0]} set out on a {adjf[5]} mission."
+sentence2 = f"One day, a {adjf[3]} and {adjf[4]} young Jesuit named {namesf[0]} set out on a(n) {adjf[5]} mission."
 
 # Combining sentences into paragraphs
-
 paragraph0 = sentence0 + " " + sentence1 + " " + sentence2
 
 sentence3 = f"He was tasked with {verbingf[1]} to a remote {snounf[3]} island, known as {namesf[1]}, to establish a {adjf[6]} mission there."
@@ -103,17 +91,16 @@ sentence10 = f"Over time, the island transformed into a place of {adjf[14]} harm
 sentence11 = f"The Jesuits had successfully {verbpf[0]} the people with their {adjf[15]} wisdom, and the islanders had embraced their {snounf[10]} teachings."
 paragraph5 = sentence10 + " " + sentence11
 
+# More sentences and paragraphs
 sentence12 = f"{namesf[0]} returned to his homeland, knowing that he had made a {adjf[16]} difference in the lives of the islanders."
-sentence13 = f"The Jesuits continued their {adjf[17]} mission, spreading {snounf[11]} enlightenment wherever they go."
+sentence13 = f"The Jesuits continued their {adjf[17]} mission, spreading {snounf[11]} enlightenment wherever they went."
 paragraph6 = sentence12 + " " + sentence13
 
-sentence14 = f"And so, the story of the Jesuits and their {adjf[18]} adventures continued, as they dedicated themselves to {verbingf[4]} the world with their {adjf[19]} knowledge and {snounf[12]} faith."
-paragraph7 = sentence14
+# Add even more sentences and paragraphs to reach 100 lines
+# ...
 
-# Combine paragraphs to create the story
+# Combine all paragraphs to create the full story
+story = paragraph0 + "\n" + paragraph1 + "\n" + paragraph2 + "\n" + paragraph3 + "\n" + paragraph4 + "\n" + paragraph5 + "\n" + paragraph6 + "\n"
 
-story = paragraph0 + "\n" + paragraph1 + "\n" + paragraph2 + "\n" + paragraph3 + "\n" + paragraph4 + "\n" + paragraph5 + "\n" + paragraph6 + "\n" + paragraph7
-
-# Print the story
-
+# Print the complete story
 print(story)
